075a7b7722a3354c553cbabd474cc51f4961bfc6
Belén menganita <belenbahmerrada@gmail.com>
2020-11-24 18:06:26 -0600

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+          <h1>Welcome tu your Fitness tracker App!!</h1>  
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Routines.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Routines () =>{
+    return (
+        <h1>It is is a routine</h1>
+    )
+}
+
+export default Routines
