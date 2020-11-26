0817ef08df037726263a093741dff8079582def2
Belén menganita <belenbahmerrada@gmail.com>
2020-11-25 19:56:44 -0600

    testing my branch

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
     return (
         <div>
-          <h1>Welcome tu your Fitness tracker App!!</h1>  
+          <h1>Welcome tu your Fitness tracker App!</h1>  
         </div>
     )
 }
